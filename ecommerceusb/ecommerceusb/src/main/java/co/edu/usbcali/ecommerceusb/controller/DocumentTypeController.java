a11c20eb7381c52e2f445a3868fec81d5512435a
SharkCtts <149117514+SharkCtts@users.noreply.github.com>
2026-03-09 21:02:21 -0500

    Clase 09/03/26 - controller

--- ecommerceusb/ecommerceusb/src/main/java/co/edu/usbcali/ecommerceusb/controller/DocumentTypeController.java
+++ ecommerceusb/ecommerceusb/src/main/java/co/edu/usbcali/ecommerceusb/controller/DocumentTypeController.java
@@ -1,4 +1,18 @@
 package co.edu.usbcali.ecommerceusb.controller;
 
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
 public class DocumentTypeController {
+
+// inyección de dependencias por contructor
+
+// inyeccion de dependencias por autowired
+
+// diferencias entre cada uno y cual es más optimo
+
+// Capa service para qué sirve en springboot
+
 }
